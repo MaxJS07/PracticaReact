d36a5b17d951fb0e7b2d9cd640ef7c410bf81329
MaxJS07 <maxjimenezsep@gmail.com>
2026-03-12 12:10:34 -0600

    Primeros componentes

---
+++ ejercicio-max-1/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({text}) => {
+    return(
+        <button type="button" class="btn btn-outline-secondary">{text}</button>
+    )
+}
+
+export default Button;
