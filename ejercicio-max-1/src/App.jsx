090a6c7042bae69b90cbcb696771c4d7ec72df8c
MaxJS07 <maxjimenezsep@gmail.com>
2026-03-17 00:01:40 -0600

    Feat: Más componentes (Radio, checks, select)

--- ejercicio-max-1/src/App.jsx
+++ ejercicio-max-1/src/App.jsx
@@ -6,9 +6,19 @@ import Button from './components/Button'
 import AccordionItem from './components/AcorddionItem'
 import Card from './components/Card'
 import TextInput from './components/TextInput'
+import Select from './components/Select'
+import Checkbox from './components/Checkbox'
 import './App.css'
+import RadioButton from './components/RadioButton'
 
 function App() {
+
+
+  const selectData = [
+    {value: "ES", text: "El Salvador"},
+    {value: "HN", text: "Honduras"},
+    {value: "US", text: "Estados Unidos"}
+  ]
 
   const accordionData = [
     {
@@ -38,6 +48,34 @@ function App() {
       title: "Inputs de texto",
       content: <div>
         <TextInput id="email" type="email" label="Correo" placeholder="ejemplo@gmail.com"></TextInput>
+        <TextInput id="name" type="text" label="Nombre" placeholder="Ingrese su nombre"></TextInput>
+      </div>
+    },
+    {
+      id: "collapseFour",
+      title: "Selects",
+      content: <div>
+        <Select id="Select1" options={selectData}></Select>
+      </div>,
+      open: false
+    },
+    {
+      id: "collapseFive",
+      title: "Checkboxes",
+      content: <div>
+        <Checkbox id="Checkbox1" label="Checkbox número 1"></Checkbox>
+        <Checkbox id="Checkbox2" label="Checkbox número 2"></Checkbox>
+        <Checkbox id="Checkbox3" label="Checkbox número 3"></Checkbox>
+      </div>
+    },
+    {
+      id: "collapseSix",
+      title: "Radio buttons",
+      content: <div>
+        <RadioButton id="Radio1" label= "Radio button 1" groupName="radioGroup1"></RadioButton>
+        <RadioButton id="Radio2" label= "Radio button 2" groupName="radioGroup1"></RadioButton>
+        <RadioButton id="Radio3" label= "Radio button 3" groupName="radioGroup1"></RadioButton>
+        <RadioButton id="Radio4" label= "Radio button 4" groupName="radioGroup1"></RadioButton>
       </div>
     }
   ]
@@ -48,7 +86,7 @@ function App() {
         <p className="text-center fs-2 fw-bold text-primary">EJERCICIO 1 - COMPONENTES</p>
       </div>
       
-      <div className="accordion mx-2" id="accordionExample">
+      <div className="accordion mx-5" id="accordionExample">
         {accordionData.map((item) =>(
           <AccordionItem
             key= {item.id}
